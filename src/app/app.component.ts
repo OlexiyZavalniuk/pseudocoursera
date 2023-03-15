93956b1a8814f7c02de93a78452d8cf66effb7e0
Olexiy Zavalniuk <83496121+OlexiyZavalniuk@users.noreply.github.com>
2023-03-15 21:06:12 +0200

    Готова перша сторінка з пагінацією (але з маштабованістю трохи біда)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ICourse } from './models/course';
 import { CourseService } from './services/course-service';
-
+import { ceil } from 'mathjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,10 +11,20 @@ export class AppComponent {
   constructor(private service: CourseService) {
     this.service.getCourses().subscribe((request) => {
       this.courses = request.courses;
+      this.pageCount = ceil(this.courses.length / 10);
     });
   }
 
   title = 'pseudocoursera';
   uncovered: boolean = false;
   courses: ICourse[] = [];
+  page: number = 1;
+  pageCount: number = 1;
+
+  getPage(): ICourse[] {
+    return this.courses.slice(
+      0 + 10 * (this.page - 1),
+      10 + 10 * (this.page - 1)
+    );
+  }
 }
